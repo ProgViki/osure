8660be72bc187facb289f82e54cff2aedce2b909
Victor Ogunwehin <victorogunwehin@gmail.com>
2025-07-13 04:37:33 +0100

    first

---
+++ app/favorites.tsx
@@ -0,0 +1,11 @@
+// app/favorites.tsx
+import { Text, View } from 'react-native';
+
+export default function FavoritesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{fontWeight: 'bold', fontSize: 24}}>Favorites Screen</Text>
+      <Text>Coming soon</Text>
+    </View>
+  );
+}
---
+++ app/history.tsx
@@ -0,0 +1,11 @@
+// app/favorites.tsx
+import { View, Text } from 'react-native';
+
+export default function HistoryScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{fontWeight: 'bold', fontSize: 24}}>History Screen</Text>
+      <Text>Coming soon</Text>
+    </View>
+  );
+}
